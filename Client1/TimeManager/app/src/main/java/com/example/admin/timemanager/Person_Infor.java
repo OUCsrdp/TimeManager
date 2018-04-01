cd11481ec756cccb8d0962c8da16b571fdddc17d
halfmoontonight <1124905486@qq.com>
2018-04-01 21:44:18 +0800

    add client pages

--- Client1/TimeManager/app/src/main/java/com/example/admin/timemanager/Person_Infor.java
+++ Client1/TimeManager/app/src/main/java/com/example/admin/timemanager/Person_Infor.java
@@ -4,11 +4,14 @@ import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
@@ -18,9 +21,9 @@ import android.widget.TextView;
 public class Person_Infor extends AppCompatActivity {
 
     private TextView schoolPop;
-    private String[] datas={"中国海洋大学","华东师范大学","中国科技大学","南京理工大学"};
-    private WindowManager wm;
-    private DisplayMetrics dm;//使用这2个参数来获取屏幕宽高
+    private String[] schools={"中国海洋大学","华东师范大学","中国科技大学","中国海洋大学","华东师范大学","华东师范大学"};
+   // private WindowManager wm;
+    //private DisplayMetrics dm;//使用这2个参数来获取屏幕宽高
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,31 +32,28 @@ public class Person_Infor extends AppCompatActivity {
         if(actionbar!=null){
             actionbar.hide();
         }
-        wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
-        dm = new DisplayMetrics();
+        //wm = (WindowManager) this.getSystemService(Context.WINDOW_SERVICE);
+        //dm = new DisplayMetrics();
         //  构建一个popupwindow的布局
         schoolPop = (TextView) findViewById(R.id.userSchool);
         schoolPop.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                View popupView = Person_Infor.this.getLayoutInflater().inflate(R.layout.popschool, null);
-                ListView lsvMore = (ListView) popupView.findViewById(R.id.lsvMore);
-                String[] datas={"中国海洋大学","华东师范大学","中国科技大学","南京理工大学"};
-                lsvMore.setAdapter(new ArrayAdapter<String>(Person_Infor.this,R.layout.list_school, datas));
-                PopupWindow window = new PopupWindow(popupView, 400, 400);
-                //window.setAnimationStyle(R.style.popup_window_anim);
-                window.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#ffffff")));
-                window.setFocusable(true);
-                wm.getDefaultDisplay().getMetrics(dm);
-                int popWidth=(int)(dm.widthPixels/dm.density);
-                int popHeight= (int)(dm.heightPixels/dm.density);
-                window.showAtLocation(popupView, Gravity.NO_GRAVITY,(popWidth-400)/2, (popHeight-400)/2);
-                //设置可以触摸弹出框以外的区域
-                window.setOutsideTouchable(true);
-                // 更新popupwindow的状态
-                window.update();
-                //以下拉的方式显示，并且可以设置显示的位置
-                //window.showAsDropDown(schoolPop, 50, 50);
+                AlertDialog schoolDialog = new AlertDialog.Builder(Person_Infor.this,R.style.timeAlertDialog).create();
+                final View dialogView = LayoutInflater.from(Person_Infor.this)
+                        .inflate(R.layout.popschool,null);
+                //为弹出层的布局添加xml
+                ArrayAdapter<String> adapterSchool=new ArrayAdapter<String>(Person_Infor.this,R.layout.list_school,schools);
+                ListView listSchool=(ListView) dialogView.findViewById(R.id.listSchool);
+                listSchool.setAdapter(adapterSchool);
+                //为listview添加时间
+                schoolDialog.setView(dialogView);
+                schoolDialog.show();
+                Window dialogWindow = schoolDialog.getWindow();
+                WindowManager.LayoutParams lp = dialogWindow.getAttributes();
+                lp.width = DensityUtil.dip2px(Person_Infor.this,200);//宽高可设置具体大小
+                lp.height =DensityUtil.dip2px(Person_Infor.this,180);
+                schoolDialog.getWindow().setAttributes(lp);
             }
         });
 
