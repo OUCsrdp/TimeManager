4be4b88791c2a0f5719cc4f92c63ee46f475e77c
yichen <17865422138@163.com>
2018-11-12 09:11:56 +0800

    excellent student changed

--- The Client/Time_Manager/app/src/main/java/com/srdp/admin/time_manager/ui/ExcellentDistributionContentActivity.java
+++ The Client/Time_Manager/app/src/main/java/com/srdp/admin/time_manager/ui/ExcellentDistributionContentActivity.java
@@ -1,22 +1,32 @@
 package com.srdp.admin.time_manager.ui;
 
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
+/*import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+import com.ashokvarma.bottomnavigation.behaviour.BottomNavBarFabBehaviour;
+import com.ashokvarma.bottomnavigation.behaviour.BottomVerticalScrollBehavior;
+import com.ashokvarma.bottomnavigation.utils.Utils;*/
 import com.srdp.admin.time_manager.R;
 
 public class ExcellentDistributionContentActivity extends AppCompatActivity {
+
+    //private BottomNavigationBar mbottomNavigationBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_excellent_distribution_content);
 
-        //隐藏默认actionbar
-        ActionBar actionbar = getSupportActionBar();
-        if (actionbar != null) {
-            actionbar.hide();
-        }
+        //底部按钮
+        //mbottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
+        //mbottomNavigationBar.setActiveColor("#e6992f") //设置选中的颜色
+//                .setInActiveColor("#e6992f")
+//                .addItem(new BottomNavigationItem(R.drawable.like_btn, "赞"))
+//                .addItem(new BottomNavigationItem(R.drawable.collect_btn, "收藏"))
+//                .initialise();//所有的设置需在调用该方法前完成
     }
+
 }
