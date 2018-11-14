ff414f218b938b1f01a5c601b6ba2ee4e83c53c9
lizlizlizlizliz <472750237@qq.com>
2018-11-14 19:35:29 +0800

    recycleviewOK-18/11/14

--- Client1/Time_Manager/app/src/main/java/com/srdp/admin/time_manager/ui/ExcellentDistributionListActivity.java
+++ Client1/Time_Manager/app/src/main/java/com/srdp/admin/time_manager/ui/ExcellentDistributionListActivity.java
@@ -1,27 +1,34 @@
 package com.srdp.admin.time_manager.ui;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.app.Activity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Toast;
-
 import com.srdp.admin.time_manager.R;
 import com.srdp.admin.time_manager.model.moudle.ExcellentStudentsDistribution;
 import com.srdp.admin.time_manager.widget.adapters.ExcellentDistributionListAdapter;
+import com.srdp.admin.time_manager.util.HttpUtil;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ExcellentDistributionListActivity extends AppCompatActivity {
 
-    private List<ExcellentStudentsDistribution> ExcellentDistributionList = new ArrayList<>();
+    List<ExcellentStudentsDistribution> ExcellentDistributionList = new ArrayList<>();
+
+    private SwipeRefreshLayout listSwiplayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +36,10 @@ public class ExcellentDistributionListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_excellent_distribution_list);
 
         initDistributions(); // 初始化数据
+        intoView();
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.distribution_list);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        //LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
         ExcellentDistributionListAdapter listAdapter = new ExcellentDistributionListAdapter(ExcellentDistributionList);
         recyclerView.setAdapter(listAdapter);
@@ -38,7 +47,26 @@ public class ExcellentDistributionListActivity extends AppCompatActivity {
 
     }
 
-    private void initDistributions() {
+    private void intoView(){
+
+        listSwiplayout = (SwipeRefreshLayout) findViewById(R.id.listswiplayout);
+
+        //手势监听
+        listSwiplayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                //下拉刷新监听数据更新在这操作
+
+                if (listSwiplayout.isRefreshing()){//判断是否刷新
+//                    mSwiplayout.setRefreshing(false);//关掉刷新
+                }
+
+            }
+        });
+
+    }
+
+    public void initDistributions() {
 
             ExcellentStudentsDistribution apple = new ExcellentStudentsDistribution("Apple", R.drawable.login_icon1, "中国海洋大学", "计算机系", "新出炉的时间分配表，请君共赏，现在二十个字了", "2017-9-6", 129);
         ExcellentDistributionList.add(apple);
@@ -61,7 +89,11 @@ public class ExcellentDistributionListActivity extends AppCompatActivity {
             ExcellentStudentsDistribution mango = new ExcellentStudentsDistribution("Apple", R.drawable.login_icon1, "中国海洋大学", "计算机系", "新出炉的时间分配表，请君共赏，现在二十个字了", "2017-9-6", 129);
         ExcellentDistributionList.add(mango);
 
-
+//        JSONObject distriObject = new JSONObject();
+//        HttpUtil.request("","post",reqJson,new okhttp3.Callback(){
+//            @Override
+//
+//        });
 
     }
 }
