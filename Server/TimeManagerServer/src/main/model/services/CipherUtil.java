5c197dd82ca1f34543c8dc6a8b5864d676792fd5
Enidwonder <enid1999@163.com>
2018-11-19 11:45:25 +0800

    token加密
    增加CipherUtil.java,修改TokenUtil.java

---
+++ Server/TimeManagerServer/src/main/model/services/CipherUtil.java
@@ -0,0 +1,42 @@
+package main.model.services;
+
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class CipherUtil {
+
+	/**使用AES对字符串加密
+     * @param str utf8编码的字符串
+     * @param key 密钥（16字节）
+     * @return 加密结果
+     * @throws Exception
+     */
+    public static String aesEncrypt(String str, String key) throws Exception { 
+           if (str == null || key == null) return null; 
+           Cipher cipher = Cipher.getInstance("AES"); 
+           cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key.getBytes("utf-8"), "AES")); 
+           byte[] bytes = cipher.doFinal(str.getBytes("utf-8")); 
+          
+           
+           return  Base64.encodeBase64String(bytes);
+       } 
+    /**使用AES对数据解密
+     * @param bytes utf8编码的二进制数据
+     * @param key 密钥（16字节）
+     * @return 解密结果
+     * @throws Exception
+     */
+       public static String aesDecrypt(String byte1, String key) throws Exception { 
+    	   byte[] bytes = Base64.decodeBase64(byte1);
+           if (bytes == null || key == null) return null; 
+           Cipher cipher = Cipher.getInstance("AES"); 
+           cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(key.getBytes("utf-8"), "AES")); 
+           bytes = cipher.doFinal(bytes);
+           return new String(bytes, "utf-8"); 
+       } 
+
+
+}
--- Server/TimeManagerServer/src/main/model/services/TokenUtil.java
+++ Server/TimeManagerServer/src/main/model/services/TokenUtil.java
@@ -1,54 +1,66 @@
-package main.model;
+package main.model.services;
 
-import main.model.moudle.*;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
-
-import main.model.db.*;
+import java.io.UnsupportedEncodingException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
-import java.security.Security; 
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.KeyGenerator;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
+import java.util.Random;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import org.apache.commons.codec.binary.Base64;
 
 public class TokenUtil{
 	
-	//KeyGenerator 提供对称密钥生成器的功能，支持各种算法	
-	private KeyGenerator keygen;	
-	//SecretKey 负责保存对称密钥	
-	private SecretKey deskey;	
-	//Cipher负责完成加密或解密工作	
-	private Cipher c;	
-	//该字节数组负责保存加密的结果	
-	private byte[] cipherByte;
-
-	
+	public static String getRandomString(int length){
+	     String str="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	     Random random=new Random();
+	     StringBuffer sb=new StringBuffer();
+	     for(int i=0;i<length;i++){
+	       int number=random.nextInt(62);
+	       sb.append(str.charAt(number));
+	     }
+	     return sb.toString();
+	 }
 	
 	//加密TOKEN
-		public static  String encodeToken(String token,int userId) throws NoSuchAlgorithmException, NoSuchPaddingException{
+		public static  String encodeToken(String token,int userId) {
 			String final_token = userId+"_"+token;
-			/*Security.addProvider(new com.sun.crypto.provider.SunJCE());		//实例化支持DES算法的密钥生成器(算法名称命名需按规定，否则抛出异常)		
-			keygen = KeyGenerator.getInstance("AES");		//生成密钥		
-			deskey = keygen.generateKey();		//生成Cipher对象,指定其支持的DES算法		
-			c = Cipher.getInstance("AES");*/
-			return final_token;
+			
+			String key = getRandomString(16);
+			String encontent;
+			try {
+				encontent = CipherUtil.aesEncrypt(final_token,key);
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				return "fail"+e.toString();
+			}
+		
+			String back = encontent + key;
+			return back;
 			
 		}
 		//解密TOKEN
-		public static int  decodeToken(String token) {
-			String sub = token.substring(0, token.indexOf("-"));
-			int id = Integer.parseInt(sub);
-			return id;
+		public static String  decodeToken(String token1) {
+			int len = token1.length();
+			String key = token1.substring(len - 16);
+			String token = token1.substring(0, len - 16);
+			System.out.println(key);
+			System.out.println(token);
+			
+			String decontent;
+			
+			try {
+				decontent = CipherUtil.aesDecrypt(token,key);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				return "fail2"+e.toString();
+			}
+			
+			
+			String sub = decontent.substring(0, decontent.indexOf("_"));
+			return sub;
 		}
 }
