5e44ae491d639b8aa615b96af3e6ce39a47ca29b
lizlizlizlizliz <472750237@qq.com>
2019-03-14 21:51:17 +0800

    update  report_form

--- Client1/Time_Manager/app/src/main/java/com/srdp/admin/time_manager/ui/ReportFormActivity.java
+++ Client1/Time_Manager/app/src/main/java/com/srdp/admin/time_manager/ui/ReportFormActivity.java
@@ -1,9 +1,12 @@
 package com.srdp.admin.time_manager.ui;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
 import android.graphics.Color;
 import android.widget.TextView;
@@ -11,6 +14,8 @@ import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.widget.Toast;
 
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.components.Legend;
@@ -22,12 +27,15 @@ import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.github.mikephil.charting.formatter.PercentFormatter;
+
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.TreeMap;
 
 import com.srdp.admin.time_manager.R;
 import com.github.mikephil.charting.components.Legend;
+import com.srdp.admin.time_manager.util.HttpUtil;
 
 import java.util.ArrayList;
 
@@ -35,11 +43,19 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 
+import okhttp3.Call;
+import okhttp3.Response;
+
+import static com.alibaba.fastjson.JSON.parseObject;
+
 
 public class ReportFormActivity extends AppCompatActivity {
 
-    private PieChart rep_piechart;
+    private PieChart rep_piechart;//图表
 
+    private TextView rep_week;//第x周
+
+    private String week;//获取的第x周
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,9 +68,10 @@ public class ReportFormActivity extends AppCompatActivity {
         }
 
         rep_piechart = (PieChart) findViewById(R.id.rep_piechart);
-        PieData mPieData = getPieData(4, 100);
+        PieData mPieData = getPieData(6, 100);
         showChart(rep_piechart, mPieData);
 
+        rep_week = (TextView) findViewById(R.id.rep_week);
     }
 
 
@@ -103,34 +120,80 @@ public class ReportFormActivity extends AppCompatActivity {
      */
     private PieData getPieData(int count, float range) {
 
-        ArrayList<String> xValues = new ArrayList<>();  //xVals用来表示每个饼块上的内容
+        final ArrayList<String> xValues = new ArrayList<>();  //xVals用来表示每个饼块上的内容
+        final ArrayList<PieEntry> yValues = new ArrayList<>();  //yVals用来表示封装每个饼块的实际数据
 
-        for (int i = 0; i < count; i++) {
-            xValues.add("Quarterly" + (i + 1));  //饼块上显示成Quarterly1, Quarterly2, Quarterly3, Quarterly4
-        }
+        //从日历页面获取日期数据
+        Intent intent=getIntent();
+        String today=intent.getStringExtra("today");
 
-        ArrayList<PieEntry> yValues = new ArrayList<>();  //yVals用来表示封装每个饼块的实际数据
+        //从后端获取数据
+        JSONObject reportObject = new JSONObject();
+        reportObject.put("date",today);
+
+        HttpUtil.request("SheetServlet?method=GetWeeklySheet","post",reportObject,new okhttp3.Callback(){
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response){
+                try{
+                    //获取服务器端响应数据
+                    String jsonString = response.body().string();
+                    Log.i("jsonString",jsonString);
+                    //获得星期x
+                    week = parseObject(jsonString).getString("weekday");
+                    rep_week.setText(week);
+                    //时间分配表
+                    JSONArray timesharing = parseObject(jsonString).getJSONArray("TimeSharing");
+                    Log.i("timesharing",timesharing.toString());
+
+                    for(int i=0;i<timesharing.size();i++){
+                        JSONObject resJsonItem = timesharing.getJSONObject(i);
+                        int labelid = resJsonItem.getIntValue("labelid");
+                        float percent = resJsonItem.getFloatValue("percent");//所占时间比，以浮点数表示
+                        String duration = resJsonItem.getString("duration");//该天所有该标签的事件总时间
+                        xValues.add("Quarterly" +labelid);
+                        yValues.add(new PieEntry(percent, duration));
+                        //xValues.add("Quarterly" + (i + 1));  //饼块上显示成Quarterly1, Quarterly2, Quarterly3, Quarterly4
+                    }
+
+                } catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e){
+                //createFail();
+            }
+        });
+
+
+
+
+//        for (int i = 0; i < count; i++) {
+//            xValues.add("Quarterly" + (i + 1));  //饼块上显示成Quarterly1, Quarterly2, Quarterly3, Quarterly4
+//        }
+
+
 
         // 饼图数据
         /**
          * 将一个饼形图分成四部分， 四部分的数值比例为14:14:34:38
          * 所以 14代表的百分比就是14%
          */
-        float quarterly1 = 13;
-        float quarterly2 = 21;
-        float quarterly3 = 11;
-        float quarterly4 = 14;
-        float quarterly5 = 15;
-        float quarterly6 = 21;
-        float quarterly7 = 23;
-
-        yValues.add(new PieEntry(quarterly1, 0));
-        yValues.add(new PieEntry(quarterly2, 1));
-        yValues.add(new PieEntry(quarterly3, 2));
-        yValues.add(new PieEntry(quarterly4, 3));
-        yValues.add(new PieEntry(quarterly5, 4));
-        yValues.add(new PieEntry(quarterly6, 5));
-        yValues.add(new PieEntry(quarterly7, 6));
+//        float quarterly1 = 13;
+//        float quarterly2 = 21;
+//        float quarterly3 = 11;
+//        float quarterly4 = 14;
+//        float quarterly5 = 15;
+//        float quarterly6 = 21;
+//        float quarterly7 = 23;
+//
+//        yValues.add(new PieEntry(quarterly1, 0));
+//        yValues.add(new PieEntry(quarterly2, 1));
+//        yValues.add(new PieEntry(quarterly3, 2));
+//        yValues.add(new PieEntry(quarterly4, 3));
+//        yValues.add(new PieEntry(quarterly5, 4));
+//        yValues.add(new PieEntry(quarterly6, 5));
+//        yValues.add(new PieEntry(quarterly7, 6));
 
         //y轴的集合
         PieDataSet pieDataSet = new PieDataSet(yValues, " "/*显示在比例图上*/);
