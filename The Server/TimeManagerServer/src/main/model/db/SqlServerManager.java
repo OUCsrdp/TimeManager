cf65145d9812e72f1c4145a46965b675b9b98e2d
lizlizlizlizliz <472750237@qq.com>
2019-05-06 08:28:06 +0800

    Update SqlServerManager.java

--- The Server/TimeManagerServer/src/main/model/db/SqlServerManager.java
+++ The Server/TimeManagerServer/src/main/model/db/SqlServerManager.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class SqlServerManager {
 	
-	private static String url = "jdbc:sqlserver://47.103.6.104:1433;databaseName=TimeManager";
+	private static String url = "jdbc:sqlserver://localhost:1433;databaseName=TimeManager";
 	//118.89.242.245:·şÎñÆ÷ipµØÖ·
 	private static String username = "root";
 	private static String password = "password";
